cedaf2a368fadaf6cc580a3e87666ba975b98253
Radon222 <89600373712@mail.ru>
2026-09-09 09:18:23 +0300

    add docker

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module study
 
-go 1.27.0
+go 1.24.0
--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"study/feature1"
-	"study/feature2"
-)
-
 func main() {
-	fmt.Println("Hello World!")
-	feature1.Feature1()
-	feature2.Feature2()
+	// fmt.Println("Запуcк HTTP сервера!")
+	// fmt.Println("Новый вывод!")
+	// err := http_server.StartHTTPServer()
+	// if err != nil {
+	// 	fmt.Println("Во время работы сервера произошла ошибка",err)
+	// } else {
+	// 	fmt.Println("Сервер завершился успешно!")
+	// }
 }
